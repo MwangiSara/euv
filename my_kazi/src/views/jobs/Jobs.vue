060c14bb45acd213b421c1f34a1fc838fdbf9a5c
sarah Mwangi <mwangisarah113@gmail.com>
2024-05-14 14:34:37 +0300

    folder structures

---
+++ my_kazi/src/views/jobs/Jobs.vue
@@ -0,0 +1,25 @@
+<template>
+  <h1>Jobs</h1>
+  <div v-for="job in jobs" :key="job.id">
+    {{ job.title }}
+  </div>
+</template>
+
+<script>
+export default {
+data(){
+    return{
+        jobs: [
+            {title:'UX Designer', id: 1, details: 'lorem'},
+            {title:'Web Developer', id: 2, details: 'lorem'},
+            {title:'Vue developer', id: 3, details: 'lorem'},
+
+        ]
+    }
+}
+}
+</script>
+
+<style>
+
+</style>
