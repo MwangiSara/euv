4604d4692282fc63eedc5ae7779c1dbbc4564088
sarah Mwangi <mwangisarah113@gmail.com>
2024-05-21 08:09:59 +0300

    fetching and displaying data

--- my_kazi/src/views/jobs/JobDetails.vue
+++ my_kazi/src/views/jobs/JobDetails.vue
@@ -1,7 +1,15 @@
 <template>
   <h1>Job Details</h1>
   <!-- <p>Job Id is: {{ $route.params.id }}</p> -->
-  <p>Job Id is: {{ id }}</p>
+  <div v-if="job">
+    <h2>{{ job.title }}</h2>
+    <p>Job Id is: {{ id }}</p>
+    <p>{{ job.details }}</p>
+  </div>
+  <div v-else>
+    <p>Loading Jobs Details...</p>
+  </div>
+  
   <!-- $route: This refers to the current route object accessible within Vue components. -->
   <!-- .params: This is a property on the $route object that holds an object containing all the route parameters extracted from the URL. -->
 </template>
@@ -12,8 +20,16 @@ export default {
   data(){
     return{
       // id: this.$route.params.id //no need for this line coz we are geting the id as a prop
+      job : null
     }
-  }
+  },
+  mounted(){
+  fetch('http://localhost:3000/jobs/' + this.id) 
+  //the code uses the fetch API to make an HTTP GET request to the URL http://localhost:3000/jobs
+    .then(resource => resource.json())
+    .then(data => this.job = data)
+    .catch(error => console.log(error.message))
+}
 }
 </script>
 
--- my_kazi/src/views/jobs/Jobs.vue
+++ my_kazi/src/views/jobs/Jobs.vue
@@ -1,23 +1,35 @@
 <template>
   <h1>Jobs</h1>
-  <div v-for="job in jobs" :key="job.id" class="job">
-    <router-link :to="{ name: 'JobDetails', params: {id: job.id}}"> <h2>{{ job.title }} </h2></router-link>
+  <div v-if="jobs.length">
+    <div v-for="job in jobs" :key="job.id" class="job">
+      <router-link :to="{ name: 'JobDetails', params: {id: job.id}}"> <h2>{{ job.title }} </h2></router-link>
     <!-- name: This property specifies the name of a route you have defined in your router configuration. Here, it's set to "JobDetails".
     params (optional): This property is an optional object that allows you to pass parameters to the target route. In this case, it's used to dynamically set the id parameter. -->
+    </div>
   </div>
+  <div v-else>
+    <p>Loading Jobs...</p>
+  </div>
+
 </template>
 
 <script>
 export default {
 data(){
     return{
-        jobs: [
-            {title:'UX Designer', id: 1, details: 'lorem'},
-            {title:'Web Developer', id: 2, details: 'lorem'},
-            {title:'Vue developer', id: 3, details: 'lorem'},
-
-        ]
+        jobs: [],
     }
+},
+// fetching data from the json-server
+mounted(){
+  fetch('http://localhost:3000/jobs') 
+  //the code uses the fetch API to make an HTTP GET request to the URL http://localhost:3000/jobs
+    .then(resource => resource.json())
+    //The .then method is used to handle the successful response from the fetch request. The first .then function takes the response object (resource) as an argument. Inside this function, the resource.json() method is called to convert the response body into a JavaScript object
+    .then(data => this.jobs = data)
+    //The second .then method takes the parsed JSON data (data) as an argument.
+    .catch(error => console.log(error.message))
+    //The .catch method is used to handle any errors that might occur during the fetch operation. It takes an error object (error) as an argument. Inside the .catch function, the code logs the error message (error.message) to the console for debugging purposes.
 }
 }
 </script>
