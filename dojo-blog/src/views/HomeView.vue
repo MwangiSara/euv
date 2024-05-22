f2e875e8c07e4cc1cabd61498e5b84ead2132726
sarah Mwangi <mwangisarah113@gmail.com>
2024-05-22 11:13:15 +0300

    reactive values using refs

--- dojo-blog/src/views/HomeView.vue
+++ dojo-blog/src/views/HomeView.vue
@@ -2,6 +2,10 @@
   <div class="home">
     <p ref="p">My name is {{ name }}, I'm {{ age }} years old</p>
     <button @click="handleClick"> Click Me</button>
+    <p>My name is {{ name2 }}, I'm {{ age2 }} years old</p>
+    <button @click="handleClick2"> Click Me2</button><br>
+    <button @click="age2++">Increase Age</button><br>
+    <input type="text" v-model="name2" placeholder=""> <!-- change Mell to another name using v-model -->
   </div>
 </template>
 
@@ -23,6 +27,10 @@ export default {
     let name = 'Sarah'
     let age = 30
 
+    //reating reactive refs so that values can change in the template
+    const name2 = ref('Mell') // by using const, it doesn't mean that the values won't change, it means the ref is a constant
+    const age2 = ref(40)
+
     const handleClick = () => {
       console.log("You have Clicked me")
       console.log(p) //outputing p to show its refs object property
@@ -30,8 +38,13 @@ export default {
       p.value.classList.add('test') //adding class 'test' to paragraph in template
       p.value.textContent = 'hello there!' //replaces text in paragragh to hello there
     }
+    const handleClick2 = () => {
+      //updating age and name with the help of refs because its reactive, it can change
+      name2.value = 'Shamy' 
+      age2.value = 55
+    }
 
-    return {name, age, handleClick,p}
+    return {name, age, handleClick,p, name2, age2, handleClick2}
   },
   created(){
     console.log('created')
